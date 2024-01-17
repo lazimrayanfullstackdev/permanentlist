cd126cce2b41570ebfd737aff6448f0255f1f038
Lazim Rayan <lazimrayanfreelancer@gmail.com>
2024-01-17 16:39:42 +0600

    Connect DB and Retrieve Data

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "Lazimrayan99*",
+  port: 5433
+});
+db.connect();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function getItems(){
+  const result = await db.query("SELECT * FROM items");
+  let items = result.rows;
+  return items;
+};
+
+app.get("/", async (req, res) => {
+  const items = await getItems();
+  console.log(items);
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
+});
+
+app.post("/add", (req, res) => {
+  const item = req.body.newItem;
+  items.push({ title: item });
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {});
+
+app.post("/delete", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
