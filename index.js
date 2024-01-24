62819cbb3548bfd630dd7b9f4c0049c9c1f86c08
Lazim Rayan <lazimrayanfreelancer@gmail.com>
2024-01-24 12:41:56 +0600

    Adding items enable and and add/retrieve from database

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  const result = await db.query("INSERT INTO items(title) VALUES ($1)",[item]);+
   res.redirect("/");
 });
 
